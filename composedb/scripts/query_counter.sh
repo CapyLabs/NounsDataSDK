7886d15280e7049fde4f788d814ac82ed4ece86b
kmangutov <kmangutov@gmail.com>
2022-12-27 13:48:13 -0800

    Update query_counter.sh

--- composedb/scripts/query_counter.sh
+++ composedb/scripts/query_counter.sh
@@ -1,2 +1,1 @@
-curl http://localhost:5001/graphql -X POST -H 'content-type: application/json'  -d '{"query":"query { counterIndex(first:10){edges { node { c
-ount } }} }"}'
+curl http://localhost:5001/graphql -X POST -H 'content-type: application/json'  -d '{"query":"query { counterIndex(first:10){edges { node { count } }} }"}'
