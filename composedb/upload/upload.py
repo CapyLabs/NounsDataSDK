4aee9756d114220cb94b9cea9f069d89b95a6535
Kirill Mangutov <kmangutov@gmail.com>
2023-01-06 18:54:18 -0800

    clean up

--- composedb/upload/upload.py
+++ composedb/upload/upload.py
@@ -1,6 +1,8 @@
 
 #  pip3 install psycopg2-binary
 import psycopg2
+
+import argparse
 import configparser
 import requests
 import json
@@ -9,111 +11,70 @@ import json
 # 2. Get the postgres table nouns_proposals rows for those proposal_ids
 # 3. Write to ComposeDb, updating those proposals, with new values 
 
-# Use The Graph API for this
 from queries import CERAMIC_PUBLISH_JSON
 from queries import CERAMIC_GET_JSON
 from queries import CERAMIC_UPDATE_JSON
+from queries import THEGRAPH_GET_PROPOSALS
+from queries import TEST_RESPONSE_THEGRAPH
 
-def call_graphql_service(ceramic_endpoint):
+DRY_RUN = False
+HEADERS = {
+    "Content-Type": "application/json"
+}
 
-    headers = {
-        "Content-Type": "application/json"
-    }
 
-    with open("ceramic_get.graphql", "r") as f:
-        query = f.read()
-
-    #payload = {
-    #    "query": query
-    #}
-    #payload = json.loads(query)
-    payload = CERAMIC_UPDATE_JSON #json.loads(CERAMIC_PUBLISH_JSON)
-
-    print(str(payload))
-    response = requests.post(ceramic_endpoint, json=payload, headers=headers)
+def call_ceramic(ceramic_endpoint):
+    response = requests.post(ceramic_endpoint, json=CERAMIC_GET_JSON, headers=HEADERS)
     response.raise_for_status()
     if response.status_code != 200:
         print(str(response))
         raise Exception("Failed to execute query")
 
     print('\n\n\n')
-    data = response.json()
+    data = response.json()['data']['nounsProposalIndex']['edges']
     print(str(data))
 
-    for key, value in data.items():
-        print(key, ":", value)
+    return data
 
 
 def call_thegraph():
-  headers = {
-      "Content-Type": "application/json"
-  }
-  payload = {"query": """{
-    proposals(orderBy: createdTimestamp, orderDirection: desc) {
-      id
-      description
-      status
-      createdTimestamp
-      abstainVotes
-      againstVotes
-      executionETA
-      forVotes
-      proposalThreshold
-      targets
-      values
-    }
-  }"""}
   url_lilnouns_thegraph = "https://api.thegraph.com/subgraphs/name/lilnounsdao/lil-nouns-subgraph"
-  response = requests.post(url_lilnouns_thegraph, json=payload, headers=headers)
+  print(str(TEST_RESPONSE_THEGRAPH))
+
+  if DRY_RUN:
+    return TEST_RESPONSE_THEGRAPH
+
+  response = requests.post(url_lilnouns_thegraph, json=THEGRAPH_GET_PROPOSALS, headers=HEADERS)
   print(str(response.json()))
+  print('DRY_RUN: ' + str(DRY_RUN))
+  return response.json()['data']['proposals']
 
-
-def read_nouns_proposals(conn):
-    cursor = conn.cursor()
-
-    with open("nouns_proposals.sql", "r") as f:
-        query = f.read()
-
-    # print(str(query))
-    cursor.execute(query)
-
-    rows = cursor.fetchall()
-    for row in rows:
-        pass
-    #    print(row)
-
-    # Get the latest blocknumbre from Ceramic. Any rows with new 
-    # blocknumbers should be written with ceramic with http call
-
-    cursor.close()
 
 def main():
+    global DRY_RUN
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dry-run', action='store_true', help='Perform a dry run without making any changes')
+    args = parser.parse_args()
+    DRY_RUN = args.dry_run
+
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    db_name = config['DEFAULT']['DatabaseName']
-    db_user = config['DEFAULT']['DatabaseUser']
-    db_password = config['DEFAULT']['DatabasePassword']
-    db_host = config['DEFAULT']['DatabaseHost']
-    db_port = config['DEFAULT']['DatabasePort']
-
     ceramic_endpoint = config['DEFAULT']['CeramicGraphQlEndpoint']
 
-    conn = None
-    try:
-        # call_graphql_service(ceramic_endpoint)
 
-        #conn = psycopg2.connect(database=db_name, user=db_user, password=db_password, host=db_host, port=db_port)
-        # read_nouns_proposals(conn)
+    latest_proposals = call_thegraph()
+    print('Loaded ' + str(len(latest_proposals)) + ' proposals from thegraph')
 
-        call_thegraph()
-        
-        conn.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    uploaded_proposals = call_ceramic(ceramic_endpoint)
+    print('Loaded ' + str(len(uploaded_proposals)) + ' proposals from ceramic')
+
+    # For each proposal
+    # Check if in ceramic by id
+    # If not in ceramic, create
+    # If in ceramic, update
+
 
 if __name__ == '__main__':
     main()
+
