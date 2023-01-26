508ea14e549970c8e8ee106417f35139ca021bec
Kirill Mangutov <kmangutov@gmail.com>
2023-01-26 09:27:32 -0800

    creater importer dir and moving from clientapi

--- clientapi/src/nounsApi.mjs
+++ clientapi/src/nounsApi.mjs
@@ -13,6 +13,62 @@ import { getSnapshot } from "./SourceSnapshot.mjs"
 import { QUERY_GET_NOUNISH_PROFILES } from "./queries.mjs"
 import { QUERY_GET_PROPOSALS } from "./queries.mjs"
 
+
+const definition = {
+  "models": {
+    "NounsProposal": {
+      "id": "kjzl6hvfrbw6c8sde0sbybhp8z3s500ru1c4ifawovkqxet5h77gjyybu1567uj",
+      "accountRelation": { "type": "list" }
+    },
+    "NounishProfile": {
+      "id": "kjzl6hvfrbw6ca02lpn94dtzq5tn7vge0t9ayomihoba1ax0erj2xgun2j8qmo6",
+      "accountRelation": { "type": "single" }
+    }
+  },
+  "objects": {
+    "NounsProposal": {
+      "ens": { "type": "string", "required": false },
+      "state": { "type": "string", "required": true },
+      "proposer": { "type": "string", "required": true },
+      "end_block": { "type": "integer", "required": true },
+      "votes_for": { "type": "integer", "required": true },
+      "blocknumber": { "type": "integer", "required": true },
+      "description": { "type": "string", "required": true },
+      "proposal_id": { "type": "integer", "required": true },
+      "start_block": { "type": "integer", "required": true },
+      "total_votes": { "type": "integer", "required": true },
+      "total_supply": { "type": "integer", "required": true },
+      "requested_eth": { "type": "integer", "required": false },
+      "votes_abstain": { "type": "integer", "required": true },
+      "votes_against": { "type": "integer", "required": true },
+      "unique_holders": { "type": "integer", "required": true },
+      "quorum_required": { "type": "integer", "required": true },
+      "transactionhash": { "type": "string", "required": true },
+      "created_timestamp": { "type": "integer", "required": true },
+      "distinct_voters_for": { "type": "integer", "required": true },
+      "total_distinct_voters": { "type": "integer", "required": true },
+      "distinct_votes_abstain": { "type": "integer", "required": true },
+      "distinct_voters_against": { "type": "integer", "required": true }
+    },
+    "NounishProfile": {
+      "time_zone": { "type": "string", "required": false },
+      "eth_address": { "type": "string", "required": true },
+      "discord_username": { "type": "string", "required": false },
+      "twitter_username": { "type": "string", "required": false },
+      "discourse_username": { "type": "string", "required": false },
+      "farcaster_username": { "type": "string", "required": false },
+      "proposal_category_preference": { "type": "string", "required": false }
+    }
+  },
+  "enums": {},
+  "accountData": {
+    "nounsProposalList": { "type": "connection", "name": "NounsProposal" },
+    "nounishProfile": { "type": "node", "name": "NounishProfile" }
+  }
+}
+
+
+/*
 const definition = {
   "models": {
     "NounsProposal": {
@@ -74,25 +130,11 @@ const definition_nounishProfile = {
   "accountData": {
     "nounishProfile": { "type": "node", "name": "NounishProfile" }
   }
-}
+}*/
 
-/*
-const profile = await composeClient.executeQuery(`
-query { nounishProfileIndex(last:1000) {edges { node { 
-  id 
-  discord_username
-  proposal_category_preference
-  farcaster_username
-}}}}
-      `);
-
-console.log(JSON.stringify(profile))*/
-
-
-//definition //as RuntimeCompositeDefinition,
 const composeClient = new ComposeClient({
   ceramic: "https://nounsdata.wtf:7007",
-  definition: definition_nounishProfile 
+  definition: definition // definition_nounishProfile 
 });
 
 const authenticate = async () => {
@@ -123,28 +165,52 @@ export const getCeramicProposals = async () => {
 
 export const writeNounishProfile = async (profile) => {
   // TODO
+
+   const testWrite = await composeClient.executeQuery(`        
+         mutation {
+            createNounishProfile(input: {
+              content: {
+                discord_username: "champion 255522bore",
+                proposal_category_preference: "treasuries"
+                eth_address: "0xbabababababababababababababababababababa"
+              }
+            }) 
+            {
+              document {
+                discord_username
+                proposal_category_preference
+              }
+            }
+          }`)
+   console.log('testWriteNounishProfile: ' + JSON.stringify(testWrite))
 }
+
+export const writeProposal = async (proposal) => {
+
+}
+
 
 
 const start = async () => {
 
   await authenticate()
 
-  const profile = await getNounishProfile();
+  /*const profile = await getNounishProfile();
   console.log(JSON.stringify(profile))
 
+  console.log('\n\n\n\\n\n\n\n\\n')
 
   const proposals = await getCeramicProposals();
   console.log(JSON.stringify(proposals))
-
+*/
   const newProfile = {
     "discord_username": "champion yore",
     "proposal_category_preference": "philanthropy",
     "eth_address": "0xbabababababababababababababababababababa"
   }
 
-  const result = writeNounishProfile(profile)
-  console.log(JOSN.stringify(result))
+  const result = writeNounishProfile(newProfile)
+  console.log(JSON.stringify(result))
 }
 
 start() 
