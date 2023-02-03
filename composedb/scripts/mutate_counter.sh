267a7a7e4667572ee2ea9f196dd90f71ae352c59
brnunes <brnunes@gmail.com>
2023-02-03 16:03:47 -0300

    Delete counter

--- composedb/scripts/mutate_counter.sh
+++
@@ -1,1 +0,0 @@
-curl http://localhost:5001/graphql -X POST -H 'content-type: application/json'  -d '{"query":"mutation { createCounter(input: { content: {count: 3}}){ document {count} }  }"}'
--- composedb/scripts/query_counter.sh
+++
@@ -1,1 +0,0 @@
-curl http://localhost:5001/graphql -X POST -H 'content-type: application/json'  -d '{"query":"query { counterIndex(first:10){edges { node { count } }} }"}'
