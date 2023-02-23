cab466c02514c35b9c073d3c0808331870760716
Kirill Mangutov <kmangutov@gmail.com>
2023-02-22 22:43:10 -0300

    maybe fix absolue vs relative locatin for buildsh on aws

--- composedb/models/build.sh
+++ composedb/models/build.sh
@@ -3,34 +3,34 @@ target_dir = 'generated2' #'generated'
 echo 'Creating and deploying'
 
 echo '- nouns_proposal.graphql'
-composedb composite:create nouns_proposal.graphql --output=$target_dir/nouns_proposal_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
-composedb composite:deploy $target_dir/nouns_proposal_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
+composedb composite:create nouns_proposal.graphql --output=./$target_dir/nouns_proposal_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
+composedb composite:deploy ./$target_dir/nouns_proposal_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
 
 read -p "Please update nouns_proposal_vote.graphql with the new nouns_proposal model id"
 
 echo '- nouns_proposal_vote.graphql'
-composedb composite:create nouns_proposal_vote.graphql --output=$target_dir/nouns_proposal_vote_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
-composedb composite:deploy $target_dir/nouns_proposal_vote_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
+composedb composite:create nouns_proposal_vote.graphql --output=./$target_dir/nouns_proposal_vote_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
+composedb composite:deploy ./$target_dir/nouns_proposal_vote_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
 
 read -p "Please update nouns_proposal_vote_relationship.graphql with the new nouns_proposal model id AND nouns_proposal_vote model id"
 
-composedb composite:create nouns_proposal_vote_relationship.graphql --output=$target_dir/nouns_proposal_vote_relationship_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
-composedb composite:deploy $target_dir/nouns_proposal_vote_relationship_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
+composedb composite:create nouns_proposal_vote_relationship.graphql --output=./$target_dir/nouns_proposal_vote_relationship_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
+composedb composite:deploy ./$target_dir/nouns_proposal_vote_relationship_composite.json -k=bae843b976859f69c37ea6ee66006d54e20f1de456f60e4338a6b47d2648c688
 
 read -p "Please check there is no errors so far"
 
 
 echo 'Merging composites'
-composedb composite:merge $target_dir/merged_composites.json $target_dir/nouns_proposal_composite.json --output=$target_dir/merged_composites.json
-composedb composite:merge $target_dir/merged_composites.json $target_dir/nouns_proposal_vote_composite.json --output=$target_dir/merged_composites.json
-composedb composite:merge $target_dir/merged_composites.json $target_dir/nouns_proposal_vote_relationship_composite.json --output=$target_dir/merged_composites.json
+composedb composite:merge ./$target_dir/merged_composites.json ./$target_dir/nouns_proposal_composite.json --output=./$target_dir/merged_composites.json
+composedb composite:merge ./$target_dir/merged_composites.json ./$target_dir/nouns_proposal_vote_composite.json --output=./$target_dir/merged_composites.json
+composedb composite:merge ./$target_dir/merged_composites.json ./$target_dir/nouns_proposal_vote_relationship_composite.json --output=./$target_dir/merged_composites.json
 
 
 read -p "Please check there is no errors so far"
 
 
 echo 'Compiling merged composite'
-composedb composite:compile $target_dir/merged_composite.json $target_dir/merged_runtime_composite.json
+composedb composite:compile ./$target_dir/merged_composite.json ./$target_dir/merged_runtime_composite.json
 
 echo 'Remaining action items:'
 echo '- copy merged_runtime_composite into /library/src/__generated__/ and restart graphql if needed'
